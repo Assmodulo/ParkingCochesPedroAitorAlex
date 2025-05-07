b3f902147b09fe861c57ede74dd4935faad8a869
COMPU-S026\daw1 <asainzs06@educantabria.es>
2025-05-07 10:24:50 +0200

    Finalizada la clase Singleton de Conexión a la Base de Datos y el archivo en el que se guardan los datos necesarios para realizar la conexión

--- JavaFX/Parking-Vehiculos-Alquiler/src/main/java/module-info.java
+++ JavaFX/Parking-Vehiculos-Alquiler/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.parkingvehiculosalquiler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.parkingvehiculosalquiler to javafx.fxml;
