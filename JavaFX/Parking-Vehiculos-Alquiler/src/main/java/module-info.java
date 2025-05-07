0f0f47ac90e07a6b5a8f66d12669358b91a2f2ac
COMPU-S033\daw1 <kikstark>
2025-05-07 09:33:53 +0200

    no message

--- JavaFX/Parking-Vehiculos-Alquiler/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.parkingvehiculosalquiler {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.parkingvehiculosalquiler to javafx.fxml;
-    exports org.example.parkingvehiculosalquiler;
-}
