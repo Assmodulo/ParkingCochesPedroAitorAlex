a3414bf58e45926d2033fe7a3d82416a1470da1d
Aitor_Clase <p3976005@gmail.com>
2025-05-16 08:26:46 +0200

    validaciones de varchars de 50 100 y 30

---
+++ TuCocheAlquileres/src/main/java/org/example/tucochealquileres/validacionesGenerales.java
@@ -0,0 +1,40 @@
+package org.example.tucochealquileres;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validacionesGenerales {
+    public static boolean validarVarchar50(String varchar50) {
+
+        //Defino el patrón de los varchar de 50
+        Pattern patronVarchar50 = Pattern.compile("[a-z A-Z]{50}");
+        //Comprobamos el String recibido como parámetro con el patrón que hemos definido
+        Matcher matcher = patronVarchar50.matcher(varchar50);
+        //Retornamos el valor de la comprobación, que será true en caso de ser correcto, false en caso contrario
+        return matcher.matches();
+    }
+
+
+
+    public static boolean validarVarchar100(String varchar100) {
+
+        //Defino el patrón de los varchar de 100
+        Pattern patronVarchar100 = Pattern.compile("[a-z A-Z]{100}");
+        //Comprobamos el String recibido como parámetro con el patrón que hemos definido
+        Matcher matcher = patronVarchar100.matcher(varchar100);
+        //Retornamos el valor de la comprobación, que será true en caso de ser correcto, false en caso contrario
+        return matcher.matches();
+    }
+
+    public static boolean validarVarchar30(String varchar30) {
+
+        //Defino el patrón de los varchar de 30
+        Pattern patronVarchar30 = Pattern.compile("[a-z A-Z]{30}");
+        //Comprobamos el String recibido como parámetro con el patrón que hemos definido
+        Matcher matcher = patronVarchar30.matcher(varchar30);
+        //Retornamos el valor de la comprobación, que será true en caso de ser correcto, false en caso contrario
+        return matcher.matches();
+    }
+
+
+}
