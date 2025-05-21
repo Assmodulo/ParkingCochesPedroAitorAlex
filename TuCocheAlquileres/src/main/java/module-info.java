2c3bd6866b32cec77a9cb82b4e7b870e962adb8e
COMPU-S033\daw1 <kikstark>
2025-05-21 09:13:06 +0200

    Pedro dandolo todo

--- TuCocheAlquileres/src/main/java/module-info.java
+++ TuCocheAlquileres/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.tucochealquileres {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens org.example.tucochealquileres to javafx.fxml;
     exports org.example.tucochealquileres;
--- TuCocheAlquileres/src/main/java/org/example/tucochealquileres/SingletonConnectionSQL.java
+++ TuCocheAlquileres/src/main/java/org/example/tucochealquileres/SingletonConnectionSQL.java
@@ -1,4 +1,4 @@
-package org.example.parkingvehiculosalquiler;
+package org.example.tucochealquileres;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
