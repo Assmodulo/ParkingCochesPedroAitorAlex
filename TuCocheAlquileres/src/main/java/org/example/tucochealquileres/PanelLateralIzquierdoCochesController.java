0c850a38fdd660f2ddf02dc7be31abdf5e95acc0
COMPU-S033\daw1 <kikstark>
2025-05-26 11:30:39 +0200

    Paneles necesarios para el java fx

--- TuCocheAlquileres/src/main/java/org/example/tucochealquileres/PanelLateralIzquierdoCochesController.java
+++ TuCocheAlquileres/src/main/java/org/example/tucochealquileres/PanelLateralIzquierdoCochesController.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
-public class PanelLateralIzquierdoParkingsController {
+public class PanelLateralIzquierdoCochesController {
 
 
     private ventanaPrincipalController ventanaPrincipalController;
